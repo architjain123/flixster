3fa8b10a2f219db300a0a7f1d638952df2d77332
Archit <archit.jaincs123@gmail.com>
2020-09-03 18:35:51 +0530

    Added Tab bar controller and collection view for superhero movies

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Archit Jain on 9/3/20.
+//  Copyright © 2020 archit. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
